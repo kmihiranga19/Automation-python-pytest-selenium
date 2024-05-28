6a218410faeb0210f676dd768cd2b44cd971085a
Kavindu <kmvithanage48@gmail.com>
2024-05-28 22:36:26 +0530

    updates

--- test_amazon_search.py
+++ test_amazon_search.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +13,10 @@ driver.maximize_window()
 driver.get("https://www.aliexpress.com/")
 
 
+def remove_popups_message():
+    try:
+        pop_close_btn = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "pop-close-btn")))
+        pop_close_btn.click()
 
-
-
+    except NoSuchElementException:
+        pass
