9aa3788a625fcece79d9430dcbeb09bd84620fd4
Kavindu <kmvithanage48@gmail.com>
2024-05-29 22:44:58 +0530

    updates

--- test_amazon_search.py
+++ test_amazon_search.py
@@ -4,13 +4,14 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import configuration
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 20)
 driver.maximize_window()
-driver.get("https://www.aliexpress.com/")
+driver.get(configuration.BaseURL)
 
 
 def remove_popups_message():
@@ -20,3 +21,16 @@ def remove_popups_message():
 
     except NoSuchElementException:
         pass
+
+    def remove_popup_notification():
+        try:
+            do_not_do_allow = driver.find_element(By.CSS_SELECTOR, 'img[src="https://img.alicdn.com/tfs/TB1GSux3fb2gK0jSZK9XXaEgFXa-21-21.png"]')
+            do_not_do_allow.click()
+
+        except NoSuchElementException:
+            print("No")
+
+    remove_popup_notification()
+
+
+remove_popups_message()
